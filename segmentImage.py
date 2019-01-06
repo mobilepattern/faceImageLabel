db93697ca74583ffd18d011e823245da9c56acf3
Russell Owen <info@mobilepattern.com>
2019-01-06 08:39:37 -0800

    Fixed segmentation of people into separate images.

--- segmentImage.py
+++ segmentImage.py
@@ -12,19 +12,25 @@ def findElements(root, patt):
 
 
 class segment:
-	def __init__(self, name, x0, y0, xh, yh):
+	'''Describes subsections of an image.
+
+	Origin is in the upper left.
+	'''
+
+	def __init__(self, name, x0, y0, x1, y1):
 		self.name = name
 		self.x0 = x0
-		self.x1 = x0 + xh
+		self.x1 = x1
 
 		self.y0 = y0
-		self.y1 = y0 + yh
+		self.y1 = y1
 
 
 	def __str__(self):
 		return "%s: x0=%s, y0=%s, x1=%s, y1=%s" % (self.name, self.x0, self.y0, self.x1, self.y1)
 
-	def writeSegment(self, original_image):
+	def writeSegment(self, original_file):
+		(h, w, d) = original_image.shape
 		x0 = int(self.x0*w)
 		y0 = int(self.y0*h)
 		x1 = int(self.x1*w)
@@ -50,19 +56,25 @@ def createSegments(filename):
 
 		for r in regions:
 			name = r.attrib['{http://ns.microsoft.com/photo/1.2/t/Region#}PersonDisplayName']
-			region = r.attrib['{http://ns.microsoft.com/photo/1.2/t/Region#}Rectangle'].split(',')
-			segments[name] = segment(name, 
-			                         float(region[0]), float(region[1]), float(region[2]), float(region[3]))
+			(x0, y0, xh, yh) = map(float, r.attrib['{http://ns.microsoft.com/photo/1.2/t/Region#}Rectangle'].split(','))
+			segments[name] = segment(name, x0, y0, x0 + xh, y0 + yh)
 	return segments
 
 filename = 'Family Picture.jpg'
+original_image = cv2.imread(filename)
 
 segments = createSegments(filename)
-
-img = cv2.imread(filename)
-(w, h, d) = img.shape
-
-print(w, h, d)
-
 for s in segments.values():
-	s.writeSegment(img)
+	s.writeSegment(original_image)
+
+# Test the segmentation write code
+
+# upperLeft = segment('upperLeft', 0.0, 0.0, 0.5, 0.5)
+# upperRight = segment('upperRight', 0.5, 0.0, 1.0, 0.5)
+# lowerLeft = segment('lowerLeft', 0.0, 0.5, 0.5, 1.0)
+# lowerRight = segment('lowerRight', 0.5, 0.5, 1.0, 1.0)
+
+# upperLeft.writeSegment(original_image)
+# upperRight.writeSegment(original_image)
+# lowerLeft.writeSegment(original_image)
+# lowerRight.writeSegment(original_image)
